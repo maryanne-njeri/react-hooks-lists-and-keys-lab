3989e2ee5b09973b974387be7960f506608431b0
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-07-15 16:01:03 +0300

    commit tree 488203fc57bff3a689c6fdc04b2ff4aea1040c9c

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,16 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
+  const projItem = projects.map((item) => 
+  <ProjectItem key={item.id} name={item.name} about={item.about} technologies={item.technologies} />
+  )
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{/* render ProjectItem components here */ }
+        {projItem}
+      </div>
     </div>
   );
 }
